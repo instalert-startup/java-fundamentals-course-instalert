287b212d34ed2c6c7ce3e94ee20d476e4dc49692
PieroMFAL <43123693+PieroMFAL@users.noreply.github.com>
2026-06-18 14:19:22 +0200

    Add Operadores.java for basic arithmetic operations

---
+++ completed-examples/lesson-2-operators-conditionals-loops/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+        System.out.println("Residuo: " + (a % b));
+    }
+}
